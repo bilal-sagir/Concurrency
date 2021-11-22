c4d3334adc108df0f5a030f62fbb74424fd36ac4
bilal-sagir <sagir.bilal@icloud.com>
2021-11-22 17:14:47 +0300

    adding delay exercise

--- Concurrency/ViewController.swift
+++ Concurrency/ViewController.swift
@@ -2,12 +2,29 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    @IBOutlet weak var myLabel : UILabel!
+    
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        DelayFunc()
     }
 
+ 
+    
+    func DelayFunc()
+    {
+        let pausedTime = 5.0
+        DispatchQueue.main.asyncAfter(deadline: .now() + pausedTime)
+        {
+            [weak self] in
+            guard let self = self else {return}
+            
+            
+            self.myLabel.text = "It s happening after 5 sec"
+        }
+    }
 
 }
 
