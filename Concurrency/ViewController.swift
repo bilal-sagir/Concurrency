f9807fb70a29bf70c06902398266cb2ae0d13a44
bilal-sagir <sagir.bilal@icloud.com>
2021-11-22 18:58:26 +0300

    http

--- Concurrency/ViewController.swift
+++ Concurrency/ViewController.swift
@@ -1,13 +1,55 @@
 import UIKit
 
+struct MyNotification
+{
+    static let dowloadEnded = Notification.Name("com.bilal-sagir.app.kvo")
+}
+
+
+class AppData
+{
+    class func notify ()
+    
+    {
+        DispatchQueue.global(qos: DispatchQoS.QoSClass.background).asyncAfter(deadline: .now() + 3.0)
+        {
+            NotificationCenter.default.post(name: MyNotification.dowloadEnded,
+                                            object: nil)
+        }
+    }
+}
+
+
+
+
 class ViewController: UIViewController
 {
 
+    @IBOutlet weak var Lbl : UILabel!
+    
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(gotNotified(_:)),
+                                               name: MyNotification.dowloadEnded,
+                                               object: nil)
     }
-
-
+    
+    @objc func gotNotified (_ note: Notification)
+    {
+        DispatchQueue.main.async {
+            self.Lbl.text = "download ENDED"
+        }
+    }
+    
+    @IBAction func dowload()
+    {
+        self.Lbl.text = "Downloading...."
+        AppData.notify()
+    }
+    
 }
 
