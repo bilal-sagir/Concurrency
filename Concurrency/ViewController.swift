b32c7cd4b806e4248c401cf80f88fa76c6efe34f
bilal-sagir <sagir.bilal@icloud.com>
2021-11-23 02:07:27 +0300

    addOperation example

--- Concurrency/ViewController.swift
+++ Concurrency/ViewController.swift
@@ -2,12 +2,27 @@ import UIKit
 
 class ViewController: UIViewController
 {
-
-    override func viewDidLoad()
+    @IBOutlet weak var myImg : UIImageView!
+    
+    @IBAction func loadImg()
     {
-        super.viewDidLoad()
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 1
+        queue.isSuspended = false
+        
+        let url = URL(string: "https://apod.nasa.gov/apod/image/1507/sombrero_hubble_11472.jpg")
+        
+        queue.qualityOfService = .background
+        queue.addOperation
+        {
+            do{
+                let data = try Data(contentsOf: url!)
+                DispatchQueue.main.sync
+                {
+                    self.myImg.image = UIImage(data: data)
+                }
+            }catch{print(error.localizedDescription)}
+        }
     }
-
-
 }
 
