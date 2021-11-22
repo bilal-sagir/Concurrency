4b950ecb4aed8dafb2ad41fab5bb9e7aeceb39a9
bilal-sagir <sagir.bilal@icloud.com>
2021-11-22 17:02:22 +0300

    starting Log

--- Concurrency/ViewController.swift
+++ Concurrency/ViewController.swift
@@ -1,17 +1,11 @@
-//
-//  ViewController.swift
-//  Concurrency
-//
-//  Created by Bilal on 22.11.2021.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
