5ac35704c32d271a58cbf2a49780b4c019be3dfb
bilal-sagir <sagir.bilal@icloud.com>
2021-11-23 16:51:10 +0300

    tableView stuck problem solved

--- Concurrency/MainTableViewController.swift
+++ Concurrency/MainTableViewController.swift
@@ -2,28 +2,62 @@ import UIKit
 
 class MainTableViewController: UITableViewController
 {
+    var values : [Average] = []
+    
+    func setPlaceHolders()
+    {
+        for _ in 0..<1000{
+            let average = Average()
+            values.append(average)
+        }
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    override func viewDidLoad()
+    {
+        setPlaceHolders()
+    }
     
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int
     {
-        return 1000
+        return values.count
     }
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellId")!
         
-        cell.textLabel?.text = "\(longProcess())"
+        let thisValue = values[indexPath.row]
+        
+        if thisValue.state == .new{
+            cell.backgroundColor = .white
+            cell.textLabel?.text = "Calculating....."
+            
+            if !tableView.isDragging && !tableView.isDecelerating
+            {
+                startOperation(val: thisValue, indexPath: indexPath)
+            }
+        }
+        else
+        {
+            cell.backgroundColor = .systemGreen
+            cell.textLabel?.text = thisValue.value
+        }
+        
         return cell
     }
     
-    
-    
-    
-    
-    
-    
-    
-
 }
 
+
+extension MainTableViewController
+{
+    func startOperation(val : Average, indexPath: IndexPath)
+    {
+        
+    }
+}
